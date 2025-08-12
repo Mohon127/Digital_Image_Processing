27047e6ff486451ca0fd70d4bfa31c1a0cb38d6f
mohon127 <114308975+Mohon127@users.noreply.github.com>
2025-08-12 18:10:06 +0600

    Update 17_bit_plane_slicing.py

--- 17_bit_plane_slicing.py
+++ 17_bit_plane_slicing.py
@@ -1,37 +1,57 @@
-import matplotlib.pyplot as plt 
-import cv2 
-import numpy as np 
+"""
+Problem statement:
+    Extracts 8 bit planes from a grayscale image.
+"""
+
+#==============  Import required libraries =================================
+import matplotlib.pyplot as plt  
+import cv2                       
+import numpy as np               
+
 
 def main():
+    #============= Read the image in grayscale (0 flag = grayscale) =========
     img_gray = cv2.imread("/home/mohon/4_1/code/dip_code/images/rgb7.png", 0)
    
+    #============= List to store original image + bit planes ================
     planes = []
-    planes.append(img_gray)
+    planes.append(img_gray)  
 
+    #================= Extract bit planes from the image ====================
     planes += img_plane(img_gray)
-    planes +=  [planes[1] + planes[2] + planes[3] + planes[4] + planes[5] + planes[6] + planes[7] + planes[8]]
-    display(planes)  
+
+    #================= Reconstruct the original =============================
+    planes += [planes[1] + planes[2] + planes[3] + planes[4] +
+               planes[5] + planes[6] + planes[7] + planes[8]]
+
+    #================== Display all images ==================================
+    img_title = ['original', 'bit-1', 'bit-2', 'bit-3', 'bit-4', 'bit-5', 
+                'bit-6', 'bit-7', 'bit-8', 'reconstructed']
+    display(planes, img_title)  
 
 
-
+#===================== function for extracting 8 bit plane of an image ======
 def img_plane(img):
+    
     planes = []
 
     for i in range(8):
-        tmp = img & (1<<i)
+        tmp = img & (1 << i)
         planes.append(tmp)
     
     return planes
-    
 
-def display(img_set):
-    
+#====================  function to display all image ========================
+
+def display(img_set, img_title):
+
     for i in range(len(img_set)):
-        plt.subplot(2,5, i+1)
-        plt.imshow(img_set[i], cmap = 'gray')
-        plt.axis('off')
+        plt.subplot(2, 5, i + 1)         
+        plt.imshow(img_set[i], cmap='gray')
+        plt.title(img_title[i])  
+        plt.axis('off')                   
     
-    plt.tight_layout()
+    plt.tight_layout() 
     plt.show()
 
 
